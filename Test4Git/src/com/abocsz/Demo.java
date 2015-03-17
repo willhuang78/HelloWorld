cdf68efb77bcd0f349fb81052b94043c515072df
will <h_hf@sina.com>
2015-03-17 10:49:02 +0800

    first

---
+++ Test4Git/src/com/abocsz/Demo.java
@@ -0,0 +1,10 @@
+package com.abocsz;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Git Test";
+		System.out.println(s);
+	}
+}
